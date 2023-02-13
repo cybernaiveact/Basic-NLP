2d499773d8ac0600663f6b99df221ff66d68e9cb
cybernaiveact <110020619+cybernaiveact@users.noreply.github.com>
2023-02-13 23:41:40 +0100

    Update NPLTensorFellowNLTKwithtranslation.cpp
    There were multiple errors and some incomplete code snippets in the provided code.

--- NPLTensorFellowNLTKwithtranslation.cpp
+++ NPLTensorFellowNLTKwithtranslation.cpp
@@ -27,16 +27,16 @@ TF_SessionRunParams* params;
 
 void load_spell_check_model() {
     // Set the options for the TensorFlow session
-    TF_SetConfig(session_options, NULL, status);
+    TF_SetConfig(session_options, nullptr, status);
 
     // Load the TensorFlow SavedModel
     graph = TF_NewGraph();
     params = TF_NewSessionRunParams();
-    TF_Buffer* run_options = NULL;
+    TF_Buffer* run_options = nullptr;
     TF_Buffer* meta_graph_def = TF_NewBuffer();
     TF_SessionOptionsSetConfig(session_options, run_options, status);
     TF_SessionOptionsSetTarget(session_options, "local", status);
-    session = TF_LoadSessionFromSavedModel(session_options, run_options, "spell_check_model", NULL, 0, meta_graph_def, graph, status);
+    session = TF_LoadSessionFromSavedModel(session_options, run_options, "spell_check_model", nullptr, 0, meta_graph_def, graph, status);
     if (TF_GetCode(status) != TF_OK) {
         std::cerr << "Error loading the TensorFlow SavedModel: " << TF_Message(status) << std::endl;
         exit(1);
@@ -59,7 +59,7 @@ bool spell_checker(std::string word) {
     const std::vector<TF_Output> inputs = {TF_Output(TF_GraphOperationByName(graph, "input_ids"), 0)};
     const std::vector<TF_Tensor*> input_values = {input_tensor};
     const std::vector<TF_Output> outputs = {TF_Output(TF_GraphOperationByName(graph, "output_0"), 0)};
-    TF_SessionRun(session, params, inputs.data(), input_values.data(), inputs.size(), outputs.data(), &output_tensor, outputs.size(), NULL, 0, status);
+    TF_SessionRun(session, params, inputs.data(), input_values.data(), inputs.size(), outputs.data(), &output_tensor, outputs.size(), nullptr, 0, status);
     if (TF_GetCode(status) != TF_OK) {
         std::cerr << "Error running the TensorFlow session: " << TF_Message(status) << std::endl;
         exit(1);
@@ -77,61 +77,4 @@ bool spell_checker(std::string word) {
 }
 
 // Load the Google Translate API client
-googletrans::Translator translator;
-# Set the seed for reproducibility
-set_seed(42)
-
-# Define the prompt for text generation
-prompt = "Amidst the flotsam and jetsam of the recent economic downturn, characterized by mass job losses and economic instability, the notion of a robust economic recovery, with substantial job creation and growth, seems tenuous and elusive, raising the specter of long-term structural unemployment and chronic economic malaise."
-
-# Define the number of processes to use
-num_processes = 4
-
-# Define a function to process each token in the prompt
-def process_token(token):
-    if token.pos_ in ["NOUN", "VERB", "ADJ", "ADV"]:
-        lemma = lemmatizer.lemmatize(token.text)
-        if not spell_checker(lemma):
-            return random.choice(spell_checker.suggest(lemma))
-        else:
-            return token.text
-    else:
-        return token.text
-
-# Define a function to generate the response text
-def generate_response(prompt):
-    response = ""
-    doc = nlp(prompt)
-    processed_tokens = []
-    for token in doc:
-        processed_tokens.append(token)
-    with tf.device('/CPU:0'):
-        with Pool(processes=num_processes) as pool:
-            processed_tokens = pool.map(process_token, processed_tokens)
-    for token in processed_tokens:
-        response += token + " "
-    response = response.strip()
-    translation_dict = {}
-    for lang in ['it', 'fr', 'de']:
-        translation_dict[lang] = translator.translate(response, dest=lang).text
-    for lang, translation in translation_dict.items():
-        aligned_sent = AlignedSent(response.split(), translation.split(), Alignment([(i, i) for i in range(len(response.split()))]))
-        for i, align_pair in enumerate(aligned_sent.alignment):
-            if align_pair[0] == align_pair[1]:
-                continue
-            original_word = response.split()[align_pair[0]]
-            translated_word = translation.split()[align_pair[1]]
-            if nlp(original_word)[0].pos_ in ['NOUN', 'VERB', 'ADJ', 'ADV']:
-                processed_translated_word = process_token(nlp(translated_word)[0])
-                if not processed_translated_word or processed_translated_word == original_word:
-                    continue
-                translation = translation.replace(translated_word, processed_translated_word)
-        translation_dict[lang] = translation
-    return translation_dict
-
-# Generate the response text
-response = generate_response(prompt)
-
-# Print the final response
-print(response)
-
+googletrans
